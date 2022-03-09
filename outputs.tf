8e9a79423eb7e84a58d9ac1b7f2498a23b4d19f9
Sean Sundberg <seansund@us.ibm.com>
2022-03-09 08:45:40 -0600

    Implements initial version of module
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,14 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "rwx_storage_class" {
+  description = "The storage class to use for Read-Write-Many volumes"
+  value       = var.rwx_storage_class
+}
+
+output "file_storage_class" {
+  description = "The storage class to use for File storage volumes"
+  value       = var.file_storage_class
+}
+
+output "block_storage_class" {
+  description = "The storage class to use for Block storage volumess"
+  value       = var.block_storage_class
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "rwx_storage_class" {
+  type        = string
+  description = "The storage class to use for Read-Write-Many volumes"
+}
 
+variable "file_storage_class" {
+  type        = string
+  description = "The storage class to use for File storage volumes"
+}
+
+variable "block_storage_class" {
+  type        = string
+  description = "The storage class to use for Block storage volumes"
+}
