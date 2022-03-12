14b441d039db9428f282c5c150ea35efbc2bf8b3
Sean Sundberg <seansund@us.ibm.com>
2022-03-12 07:16:29 -0600

    Updates test logging
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -9,16 +9,16 @@ ACTUAL_FILE_SC=$(cat .file_storage_class)
 ACTUAL_BLOCK_SC=$(cat .block_storage_class)
 
 if [[ "${ACTUAL_RWX_SC}" != "${EXPECTED_RWX_SC}" ]]; then
-  echo "RWX storage class is not expected value"
+  echo "RWX storage class is not expected value: expected=${EXPECTED_RWX_SC}, actual=${ACTUAL_RWX_SC}"
   exit 1
 fi
 
 if [[ "${ACTUAL_FILE_SC}" != "${EXPECTED_FILE_SC}" ]]; then
-  echo "RWX storage class is not expected value"
+  echo "File storage class is not expected value: expected=${EXPECTED_FILE_SC}, actual=${ACTUAL_FILE_SC}"
   exit 1
 fi
 
 if [[ "${ACTUAL_BLOCK_SC}" != "${EXPECTED_BLOCK_SC}" ]]; then
-  echo "RWX storage class is not expected value"
+  echo "Block storage class is not expected value: expected=${EXPECTED_BLOCK_SC}, actual=${ACTUAL_BLOCK_SC}"
   exit 1
 fi
